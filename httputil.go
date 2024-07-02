39fdb6028eb30054d1c93492d8a599d9ce658b46
Arran Ubels <arran4@gmail.com>
2024-07-02 15:44:06 +1000

    Simple progress added.

--- httputil.go
+++ httputil.go
@@ -9,7 +9,47 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
+
+type DownloadProgress struct {
+	Resp      *http.Response
+	TotalSize *int64
+	Size      int
+	Tick      time.Time
+	Start     time.Time
+}
+
+func (d *DownloadProgress) Write(p []byte) (n int, err error) {
+	if d.Size == 0 {
+		if d.Resp != nil && d.Resp.ContentLength > 0 {
+			d.TotalSize = &d.Resp.ContentLength
+		}
+		d.Start = time.Now()
+	}
+	total := "unknown"
+	estimate := "unknown"
+	percent := "unknown%"
+	t := time.Now()
+	duration := t.Sub(d.Start)
+	d.Size += len(p)
+	if t.Sub(d.Tick) > 5*time.Second {
+		d.Tick = t
+		if d.TotalSize != nil {
+			percent = fmt.Sprintf("%03d%%", int(100*(float64(d.Size)/float64(*d.TotalSize))))
+			total = fmt.Sprintf("%d kb", *d.TotalSize/1024)
+			durationInSeconds := duration / time.Second
+			if durationInSeconds > 0 {
+				bytesPerSec := d.Size / int(durationInSeconds)
+				if bytesPerSec > 0 {
+					estimate = (time.Duration(int(*d.TotalSize)/bytesPerSec) * time.Second).String()
+				}
+			}
+		}
+		log.Printf("%s %d kb / %s (%s / %s)", percent, d.Size/1024, total, duration, estimate)
+	}
+	return len(p), nil
+}
 
 func DownloadAndChecksum(url string) (int64, string, string, error) {
 	resp, err := http.Get(url)
@@ -33,7 +73,7 @@ func DownloadAndChecksum(url string) (int64, string, string, error) {
 	}
 	sha512Hash := sha512.New()
 
-	multiWriter := io.MultiWriter(blake2bHash, sha512Hash)
+	multiWriter := io.MultiWriter(blake2bHash, sha512Hash, &DownloadProgress{Resp: resp})
 
 	sizeCh := make(chan int64)
 	errCh := make(chan error)
