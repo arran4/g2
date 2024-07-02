79491f632e7acaa944a77d752b20066ffbcf74ed
Arran Ubels <arran4@gmail.com>
2024-07-02 15:11:46 +1000

    Flag parser stuff

--- cmd/g2/main.go
+++ cmd/g2/main.go
@@ -7,34 +7,50 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type MainArgConfig struct {
+	Args []string
 }
 
 func main() {
 	fs := flag.NewFlagSet("", flag.ExitOnError)
-	config := &MainArgConfig{}
+	cfg := &MainArgConfig{
+		Args: []string{os.Args[0]},
+	}
+	fs.Usage = func() {
+		fmt.Printf("Usage:\n")
+		fmt.Printf("\t%s\n", strings.Join(cfg.Args, " "))
+		fmt.Printf("\t\t %s \t\t %s\n", "manifest", "commands relating to Manifest files")
+	}
 	if err := fs.Parse(os.Args); err != nil {
 		log.Printf("Flag parse error: %s", err)
 		os.Exit(-1)
 		return
 	}
+	cmd := "help"
 	if fs.NArg() <= 1 {
 		log.Printf("Please specify an argument, try -help for help")
 		os.Exit(-1)
 		return
+	} else {
+		cmd = fs.Arg(1)
 	}
-	switch fs.Arg(1) {
+	cfg.Args = append(cfg.Args, cmd)
+	switch cmd {
 	case "manifest":
-		if err := config.cmdManifest(fs.Args()[2:]); err != nil {
+		if err := cfg.cmdManifest(fs.Args()[2:]); err != nil {
 			log.Printf("generate error: %s", err)
 			os.Exit(-1)
 			return
 		}
 	default:
-		log.Printf("Unknown command %s", fs.Arg(1))
-		log.Printf("Try %s for %s", "manifest", "commands relating to Manifest files")
+		fmt.Printf("Unknown command %s", cmd)
+		fs.Usage()
+		return
+	case "help", "-help", "--help":
+		fs.Usage()
 		os.Exit(-1)
 	}
 }
@@ -45,28 +61,37 @@ type CmdManifestArgConfig struct {
 
 func (cfg *MainArgConfig) cmdManifest(args []string) error {
 	fs := flag.NewFlagSet("", flag.ExitOnError)
+	fs.Usage = func() {
+		fmt.Printf("Usage:\n")
+		fmt.Printf("\t%s\n", strings.Join(cfg.Args, " "))
+		fmt.Printf("\t\t %s \t\t %s\n", "upsert-from-url", "To update or insert Manifest entries streamed from a URL")
+	}
 	config := &CmdManifestArgConfig{
 		MainArgConfig: cfg,
 	}
 	if err := fs.Parse(args); err != nil {
 		return fmt.Errorf("parsing flags: %w", err)
 	}
-	switch fs.Arg(0) {
-	case "upsert-file-from-url":
-		if err := config.cmdUpsertFileFromUrl(fs.Args()[1:]); err != nil {
+	cmd := fs.Arg(0)
+	cfg.Args = append(cfg.Args, cmd)
+	switch cmd {
+	case "upsert-from-url":
+		if err := config.cmdUpsertFromUrl(fs.Args()[1:]); err != nil {
 			return fmt.Errorf("updsert file from url: %w", err)
 		}
 	default:
-		log.Printf("Unknown command %s", fs.Arg(0))
-		log.Printf("Try %s for %s", "upsert-file-from-url", "To update or insert Manifest entries streamed from a URL")
+		fs.Usage()
+		return fmt.Errorf("unknown command %s", cmd)
+	case "help", "-help", "--help":
+		fs.Usage()
 		os.Exit(-1)
 	}
 	return nil
 }
 
-func (cfg *CmdManifestArgConfig) cmdUpsertFileFromUrl(args []string) error {
+func (cfg *CmdManifestArgConfig) cmdUpsertFromUrl(args []string) error {
 	if len(args) != 4 {
-		return fmt.Errorf("usage: go run main.go <url> <filename> <Manifest file or dir>")
+		return fmt.Errorf("usage: upsert-from-url <url> <filename> <manifestFileOrDir>")
 	}
 
 	url := args[1]
